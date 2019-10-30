1d4ef5cd2f7b76801d1fb016f621ea7d5f7adef4
Eugene Nakoneschniy <evgeniy.nakoneschniy@suttermills.com>
2019-10-30 11:47:24 +0200

    first lesson (into to js and data types)

---
+++ 01/js/script.js
@@ -0,0 +1,30 @@
+// строчный комментарий
+alert('Hello world!');
+
+/*
+    блочный
+    комментарий
+*/
+
+alert(x);
+// alert(name);
+var x = 5 // old style
+alert(x);
+var x = 3 // old style
+alert(x);
+
+let name = 6; // type Number
+// name = 78;
+const z = 7.7; // type Number
+const res = name + z;
+const firstName = "Eugene"; // type String
+const firstName1 = 'Eugene1' + name; // type String
+const firstName2 = `Eugene ${name}`; // type String
+const isTrue = true; // type Boolean (true / false)
+const  testUndefined = undefined; // type undefined
+
+const Имя = "Евгений";
+
+
+
+
